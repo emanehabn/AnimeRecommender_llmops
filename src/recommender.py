19d5ba12bf26ab71ddf6fff03e5596fd9d7f7ee4
Eman-Ehab <emanehab.ieee@gmail.com>
2026-02-12 00:36:57 +0200

    Save local changes before syncing with remote

--- src/recommender.py
+++ src/recommender.py
@@ -5,7 +5,6 @@ from langchain_groq import ChatGroq
 
 from src.prompt_template import get_anime_prompt
 
-
 class AnimeRecommender:
     def __init__(self, retriever, api_key:str, model_name:str):
         self.llm = ChatGroq(api_key=api_key, model=model_name, temperature=0)
